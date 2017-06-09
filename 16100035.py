91c65a4ad8f22856e8bb12db7cd0e65126893515
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2017-06-09 14:29:19 +0530

    Added code, of Hello World, using Tkinter.
    Tkinter module, code added for "Hello, World!". Helps, when GUI required.

--- 16100035.py
+++ 16100035.py
@@ -3,4 +3,14 @@
 # 2017-06-09 Fri 2:17 PM
 
 # Python program to print Hello World
-print("Hello World\n")
+print("Hello World!\n")
+
+# For GUI, using Tkinter
+# Use Tkinter if Python2, else tkinter if python3
+
+# from Tkinter import * 
+# create root window or initialize
+# root = Tk()
+# window = Label(root, text = "Hello, World!")
+# window.pack()
+# root.mainloop()
